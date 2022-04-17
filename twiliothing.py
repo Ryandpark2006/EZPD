5c38b95d6a2e5daa2ca1a0a1d1ceca436ded50f7
Ryan Park <ryandpark2006@gmail.com>
2022-04-17 11:28:25 -0400

    github

--- twiliothing.py
+++ twiliothing.py
@@ -2,14 +2,21 @@ import os
 
 from twilio.rest import Client
 
-# To set up environmental variables, see http://twil.io/secure
-ACCOUNT_SID = "ACc67a5fbe3477b94dadea58c9b19f0d24"
-AUTH_TOKEN = "d67bc66969beafba9752247bd77f3161"
+def sendText(x):
+      # To set up environmental variables, see http://twil.io/secure
+      ACCOUNT_SID = "ACc67a5fbe3477b94dadea58c9b19f0d24"
+      AUTH_TOKEN = "d67bc66969beafba9752247bd77f3161"
 
-client = Client(ACCOUNT_SID, AUTH_TOKEN)
-notification = client.messages.create(to = "+15715282308", from_ = "+19893498993",
-#notification = client.notify.services('ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
- #   .notifications.create(
-  #      to_binding='{"binding_type":"sms", "address":"+19893498993"}',
-        body='Knok-Knok! This is your first Notify SMS')
-print(notification.sid)
+      client = Client(ACCOUNT_SID, AUTH_TOKEN)
+      notification = client.messages.create(to = "+15715282308", from_ = "+19893498993",
+      #notification = client.notify.services('ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
+      #   .notifications.create(
+      #      to_binding='{"binding_type":"sms", "address":"+19893498993"}',
+            body=x)
+      print(notification.sid)
+
+sendText('Your patient has submitted their results! Please visit https://www.ezpd.com to see your results!')   # goes to
+sendText('Your results are out! Please visit https://www.ezpd.com to see your results!')   # goes to
+
+
+
